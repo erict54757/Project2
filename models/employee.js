4e6ee70a91bb9bac23dcc9fabd0dc6a7e529f4ad
Nathaniel876 <nathanieltaylor876@gmail.com>
2018-10-26 13:09:16 -0400

    ok

---
+++ models/employee.js
@@ -0,0 +1,40 @@
+module.exports = function(sequelize, DataTypes) {
+  var Employee = sequelize.define("Employee", {
+    userName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      isAlphanumeric: true
+    },
+    password: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      isAlphanumeric: true
+    },
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      is: ["^[a-z]+$", "i"]
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      is: ["^[a-z]+$", "i"]
+    },
+    phoneNumber: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      isNumeric: true
+    },
+    hoursWorked: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      isNumeric: true,
+      max: 80
+    },
+    openInvoices: {
+      type: DataTypes.TEXT,
+      allowNull: true
+    }
+  });
+  return Employee;
+};
