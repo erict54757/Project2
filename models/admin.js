d2a44431347ac746a43e9e064f7cdec7a2e02d0e
Nathaniel876 <42044409+Nathaniel876@users.noreply.github.com>
2018-10-26 13:02:53 -0400

    Revert "m"

--- models/admin.js
+++
@@ -1,7 +0,0 @@
-module.exports = function (sequelize, DataTypes) {
-    var Admin = sequelize.define("Admin", {
-        product: {type: DataTypes.STRING, allowNull: false,  },
-        price: {type: DataTypes.DECIMAL, allowNull:false},
-    });
-    return Admin;
-};
